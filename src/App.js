ac1bea06cd361e68532aa29e44da18f8813df4fc
James Cervantes <jccervantes831@gmail.com>
2018-12-19 17:52:02 -0500

    NavBar&Routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch, } from 'react-router-dom'
 import Navbar from './components/NewNavbar'
 import Body from './components/Body'
+import About from './components/About'
+import Contact from './components/Contact'
 import './App.css';
 
 class App extends Component {
@@ -10,10 +12,15 @@ class App extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <Body/>
-        
-      </React.Fragment>
+      <Router>
+        <React.Fragment>
+          <Navbar/>
+            <Switch>
+              <Route exact path="/" Component={Body}/>
+              <Route path="/Contact" Component={Contact}/>
+            </Switch>
+        </React.Fragment>
+      </Router>
     );
   }
 }
--- src/components/NewNavbar.js
+++ src/components/NewNavbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Navbar, Nav, NavItem}  from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 
 export default class NewNavbar extends Component {
   render() {
@@ -13,10 +14,14 @@ export default class NewNavbar extends Component {
            </Navbar.Header>
            <Navbar.Toggle/>
            <Navbar.Collapse>
-          <Nav>
-            <NavItem >
+          <Nav pullRight>
+            <NavItem eventKey={1} href="/Body" componentClass={Link} to="/Body">
+              Search
             </NavItem>
-         </Nav>
+            <NavItem eventKey={2} href="/About" componentClass={Link} to="/About">
+              About
+            </NavItem>
+          </Nav>
          </Navbar.Collapse>
       </Navbar>
     )
