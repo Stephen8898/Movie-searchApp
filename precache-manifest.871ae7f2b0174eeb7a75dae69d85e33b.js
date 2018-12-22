9c2d3a9695d2bbfb0b097ee571b461aaa8b2e992
Stephen Sarfo <Stephen.sarfo1998@outlook.com>
2018-12-21 20:17:15 -0500

    Updates

---
+++ precache-manifest.871ae7f2b0174eeb7a75dae69d85e33b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f83f95413cbd623943e",
+    "url": "/Stephen8898/Project2-WebDev/static/js/runtime~main.8f83f954.js"
+  },
+  {
+    "revision": "db321a066475ea1f7e02",
+    "url": "/Stephen8898/Project2-WebDev/static/js/main.db321a06.chunk.js"
+  },
+  {
+    "revision": "0c9e3e53f1825fda72bd",
+    "url": "/Stephen8898/Project2-WebDev/static/js/1.0c9e3e53.chunk.js"
+  },
+  {
+    "revision": "db321a066475ea1f7e02",
+    "url": "/Stephen8898/Project2-WebDev/static/css/main.6379fb0d.chunk.css"
+  },
+  {
+    "revision": "9dd6f93a37c168f1204e2abc12f39574",
+    "url": "/Stephen8898/Project2-WebDev/index.html"
+  }
+];
